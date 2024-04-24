bfc96d84de095e311d42177e6e9325ca50634c54
Pierre-Louis Aublin <pl@pakupaku.me>
2024-04-24 13:57:55 +0900

    new version of my layout

--- keyboards/planck/rev6/rules.mk
+++ keyboards/planck/rev6/rules.mk
@@ -8,8 +8,10 @@ CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes       # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no      # Enable keyboard backlight functionality; non-RGB key-cap backlight
-AUDIO_ENABLE = yes           # Audio output
+AUDIO_ENABLE = no           # Audio output
 RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
+#TAP_DANCE_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
 
 RGB_MATRIX_ENABLE = no		# per-key RGB
 ENCODER_ENABLE = yes
