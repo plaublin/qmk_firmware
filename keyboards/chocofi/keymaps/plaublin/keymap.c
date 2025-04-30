966384fc70eeb6babfd2f4f17b2845a444a45676
Pierre Louis Aublin <pl@pakupaku.me>
2025-04-30 09:51:06 +0900

    mouse layer now accessible from sym layer

--- keyboards/chocofi/keymaps/plaublin/keymap.c
+++ keyboards/chocofi/keymaps/plaublin/keymap.c
@@ -37,17 +37,17 @@ enum planck_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_FOCAL] = LAYOUT_split_3x5_3(
-            KC_V, KC_L, KC_H , KC_G     , KC_K     , KC_Q     , KC_F     , KC_O  , KC_U   , KC_J   ,
-            KC_S, KC_R, KC_N , KC_T     , KC_B     , KC_Y     , KC_C     , KC_E  , KC_A   , KC_I   ,
-            KC_Z, KC_X, KC_M , KC_D     , KC_P     , KC_QUOT  , KC_W     , KC_DOT, KC_SCLN, KC_COMM,
-                        KC_NO, LT_SYMESC, MT_SFTSPC, MT_SFTENT, LT_NUMBSP, KC_NO
+            KC_V, KC_L, KC_H , KC_G     , KC_K     ,        KC_Q     , KC_F     , KC_O  , KC_U   , KC_J   ,
+            KC_S, KC_R, KC_N , KC_T     , KC_B     ,        KC_Y     , KC_C     , KC_E  , KC_A   , KC_I   ,
+            KC_Z, KC_X, KC_M , KC_D     , KC_P     ,        KC_QUOT  , KC_W     , KC_DOT, KC_SCLN, KC_COMM,
+                        KC_NO, LT_SYMESC, MT_SFTSPC,        MT_SFTENT, LT_NUMBSP, KC_NO
             ),
 
     [_QWERTY] = LAYOUT_split_3x5_3(
-            KC_Q, KC_W, KC_E , KC_R     , KC_T     , KC_Y     , KC_U     , KC_I   , KC_O  , KC_P   ,
-            KC_A, KC_S, KC_D , KC_F     , KC_G     , KC_H     , KC_J     , KC_K   , KC_L  , KC_SCLN,
-            KC_Z, KC_X, KC_C , KC_V     , KC_B     , KC_N     , KC_M     , KC_COMM, KC_DOT, KC_QUOT,
-                        KC_NO, LT_SYMESC, MT_SFTSPC, MT_SFTENT, LT_NUMBSP, KC_NO
+            KC_Q, KC_W, KC_E , KC_R     , KC_T     ,        KC_Y     , KC_U     , KC_I   , KC_O  , KC_P   ,
+            KC_A, KC_S, KC_D , KC_F     , KC_G     ,        KC_H     , KC_J     , KC_K   , KC_L  , KC_SCLN,
+            KC_Z, KC_X, KC_C , KC_V     , KC_B     ,        KC_N     , KC_M     , KC_COMM, KC_DOT, KC_QUOT,
+                        KC_NO, LT_SYMESC, MT_SFTSPC,        MT_SFTENT, LT_NUMBSP, KC_NO
             ),
 
     //Top row contains everything for the French accentued letters:
@@ -57,31 +57,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ^ then press a letter for the circonflexe accent (ê, ô, â, î, û)
     // " then press a letter for the 2 dots accent (ï, ü, ë)
     [_SYMBOLS] = LAYOUT_split_3x5_3(
-            KC_GRV       , RALT(KC_E)   , RALT(KC_GRV) , RALT(KC_COMM), RALT(KC_6), RSA(KC_QUOT), KC_LCBR, KC_RCBR, KC_AMPR, KC_NO,
-            KC_CIRC      , KC_EXLM      , KC_AT        , KC_HASH      , KC_PERC   , RSA(KC_0)   , KC_LPRN, KC_RPRN, KC_SLSH, KC_DLR,
-            OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_TAB       , KC_NO     , KC_NO       , KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
-                                          _______      , _______      , _______   , _______     , _______, _______
+            KC_GRV       , RALT(KC_E)   , RALT(KC_GRV) , RALT(KC_COMM), RALT(KC_6),         RSA(KC_QUOT), KC_LCBR, KC_RCBR, KC_AMPR, KC_NO,
+            KC_CIRC      , KC_EXLM      , KC_AT        , KC_HASH      , KC_PERC   ,         RSA(KC_0)   , KC_LPRN, KC_RPRN, KC_SLSH, KC_DLR,
+            OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_TAB       , TT(_MOUSE),         KC_NO       , KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
+                                          _______      , _______      , _______   ,         _______     , _______, _______
             ),
 
     [_MOUSE] = LAYOUT_split_3x5_3(
-            KC_NO        , KC_BTN3      , KC_WH_U      , QK_GESC , KC_NO  , KC_BTN3, KC_BTN1, KC_MS_U      , KC_BTN2      , TG(_MOUSE)   ,
-            KC_NO        , KC_BTN1      , KC_WH_D      , KC_BTN2 , KC_NO  , KC_NO  , KC_MS_L, KC_MS_D      , KC_MS_R      , OSM(MOD_RSFT),
-            OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_TAB  , KC_NO  , KC_NO  , KC_NO  , OSM(MOD_RCTL), OSM(MOD_RALT), OSM(MOD_RGUI),
-                                          _______      , _______ , _______, _______, _______, _______
+            KC_NO  , KC_BTN3, KC_WH_U, QK_GESC, KC_NO     ,         KC_BTN3, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO  ,
+            KC_NO  , KC_BTN1, KC_WH_D, KC_BTN2, KC_NO     ,         KC_NO  , KC_MS_L, KC_MS_D, KC_MS_R, KC_NO  ,
+            KC_LGUI, KC_LALT, KC_LCTL, KC_TAB , TG(_MOUSE),         KC_NO  , KC_NO  , KC_RCTL, KC_RALT, KC_RGUI,
+                              _______, _______, _______   ,         _______, _______, _______
             ),
 
     [_NUMBERS] = LAYOUT_split_3x5_3(
-            KC_INS       , KC_HOME      , KC_UP        , KC_END , KC_DEL  , KC_SLSH, KC_7   , KC_8   , KC_9, KC_ASTR,
-            QK_GESC      , KC_LEFT      , KC_DOWN      , KC_RGHT, KC_PGUP , KC_0   , KC_1   , KC_2   , KC_3, KC_MINS,
-            OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_TAB , KC_PGDN , KC_EQL , KC_4   , KC_5   , KC_6, KC_DOT ,
-                                          _______      , _______, _______, _______, _______, _______
+            KC_INS       , KC_HOME      , KC_UP        , KC_END , KC_DEL  ,         KC_SLSH, KC_7   , KC_8   , KC_9, KC_ASTR,
+            QK_GESC      , KC_LEFT      , KC_DOWN      , KC_RGHT, KC_PGUP ,         KC_0   , KC_1   , KC_2   , KC_3, KC_MINS,
+            OSM(MOD_LGUI), OSM(MOD_LALT), OSM(MOD_LCTL), KC_TAB , KC_PGDN ,         KC_EQL , KC_4   , KC_5   , KC_6, KC_DOT ,
+                                          _______      , _______, _______ ,         _______, _______, _______
             ),
 
     [_FUNCTION] = LAYOUT_split_3x5_3(
-            KC_DEL , KC_F1, KC_F2  , KC_F3  , KC_F4  , KC_MPLY, KC_VOLU, DF(_FOCAL), DF(_QWERTY)  , TT(_MOUSE),
-            QK_GESC, KC_F5, KC_F6  , KC_F7  , KC_F8  , KC_MSTP, KC_VOLD, KC_NO     , KC_NO        , KC_NO     ,
-            KC_PSCR, KC_F9, KC_F10 , KC_F11 , KC_F12 , KC_NO  , KC_MUTE, KC_NO     , OSM(MOD_RALT), QK_BOOT   ,
-                            _______, _______, _______, _______, _______, _______
+            KC_DEL , KC_F1, KC_F2  , KC_F3  , KC_F4  ,      KC_MPLY, KC_VOLU, DF(_FOCAL), DF(_QWERTY)  , KC_NO  ,
+            QK_GESC, KC_F5, KC_F6  , KC_F7  , KC_F8  ,      KC_MSTP, KC_VOLD, KC_NO     , KC_NO        , KC_NO  ,
+            KC_PSCR, KC_F9, KC_F10 , KC_F11 , KC_F12 ,      KC_NO  , KC_MUTE, KC_NO     , OSM(MOD_RALT), QK_BOOT,
+                            _______, _______, _______,      _______, _______, _______
 
             ),
 };
